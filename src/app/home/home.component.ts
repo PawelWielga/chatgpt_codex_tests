baf3fad76ea77757aea6cf34d29fc7a6bfec3ec1
PawelWielga <132073666+PawelWielga@users.noreply.github.com>
2025-06-15 00:35:50 +0200

    Extract navbar and footer components (#111)

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,14 +1,14 @@
 import { Component } from '@angular/core';
 
 import { HomeTileComponent } from '../shared/home-tile/home-tile.component';
+import { NavbarComponent } from '../shared/navbar/navbar.component';
+import { FooterComponent } from '../shared/footer/footer.component';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss'],
   standalone: true,
-  imports: [HomeTileComponent]
+  imports: [HomeTileComponent, NavbarComponent, FooterComponent]
 })
-export class HomeComponent {
-  currentYear: number = new Date().getFullYear();
-}
+export class HomeComponent {}
