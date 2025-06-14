b78922d990b9fdb08fe69044d7f3e277f2752d8b
PawelWielga <132073666+PawelWielga@users.noreply.github.com>
2025-06-15 00:18:15 +0200

    Rename game-card component to home-tile (#110)

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,13 +1,13 @@
 import { Component } from '@angular/core';
 
-import { GameCardComponent } from '../shared/game-card/game-card.component';
+import { HomeTileComponent } from '../shared/home-tile/home-tile.component';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss'],
   standalone: true,
-  imports: [GameCardComponent]
+  imports: [HomeTileComponent]
 })
 export class HomeComponent {
   currentYear: number = new Date().getFullYear();
