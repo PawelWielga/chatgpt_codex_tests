3c80ab12e03bf7cc3596b67bbdcb465d86c27664
PawelWielga <132073666+PawelWielga@users.noreply.github.com>
2025-06-15 00:06:34 +0200

    Add reusable game card component (#109)

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,11 @@
 import { Component } from '@angular/core';
 
+import { HomeComponent } from './home/home.component';
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
+  standalone: true,
+  imports: [HomeComponent]
 })
 export class AppComponent {}
